39abd2f0eb960236d22d0c77137603b10b495938
Oleg Arbuzov <oleg.arbuzov@rt.ru>
2024-10-27 19:22:59 +0300

    build: добавляет конфиги для вебкака

---
+++ webpack.config.common.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const SpriteLoaderPlugin = require('svg-sprite-loader/plugin');
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    entry: './index.js',
+    output: {
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'public/index.html'),
+        }),
+        new MiniCssExtractPlugin(),
+        new CopyPlugin({
+          patterns: [
+            { from: path.resolve(__dirname, 'src/assets'), to: path.resolve(__dirname, 'dist/assets') },
+          ],
+        }),
+        new SpriteLoaderPlugin(),
+    ],
+    module: {
+        rules: [
+          {
+            test: /\.css$/i,
+            use: [MiniCssExtractPlugin.loader, 'css-loader'],
+          },
+          {
+            test: /\.s[ac]ss$/i,
+            use: [
+                MiniCssExtractPlugin.loader,
+                'css-loader',
+                {
+                    loader: 'postcss-loader',
+                    options: {
+                      postcssOptions: {
+                        plugins: [
+                          [require('postcss-preset-env')],
+                        ],
+                      },
+                    },
+                  },
+                'sass-loader',
+            ],
+          },
+          {
+            test: /\.svg$/,
+            use: [
+              { loader: 'svg-sprite-loader'},
+            ]
+          }
+        ],
+      },
+}
